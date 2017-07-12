e983b8004f6076230c55b6b6e37986da742047f7
user2-PC\user2 <doublekick132@gmail.com>
2017-07-12 15:56:27 +0900

    recipe controller - view updated

--- src/main/java/com/njgo/controller/RecipeController.java
+++ src/main/java/com/njgo/controller/RecipeController.java
@@ -3,7 +3,9 @@ package com.njgo.controller;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.njgo.service.RecipeService;
 
@@ -13,4 +15,8 @@ public class RecipeController {
 	@Inject
 	private RecipeService recipeService;
 	
+	@RequestMapping(value="recipeView", method=RequestMethod.GET)
+	public void view(Integer num, Model model) throws Exception{
+		model.addAttribute("recipe", recipeService.view(num));
+	}
 }
